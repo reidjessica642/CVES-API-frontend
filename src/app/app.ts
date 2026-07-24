eaee75ab550bb8e3d76915669d1a4a08188f89c2
reidjessica642 <reidjessica642@gmail.com>
2026-07-23 23:11:47 -0500

    Navbar and bootstrap shenanigans, cve-listing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CveData } from "./cve-data/cve-data";
+import { CveData } from "./cve-data/cve-data.js";
+import { CveListing } from './cve-listing/cve-listing.js';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CveData],
+  imports: [RouterOutlet, CveData, CveListing],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
